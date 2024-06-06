f765490bf12efb5c826db322b968caf4fb8282da
Atanas Laskov <latanas@gmail.com>
2024-06-06 18:52:50 +0100

    Add health service actuator.

---
+++ src/main/java/com/laskov/city/CityController.java
@@ -0,0 +1,12 @@
+package com.laskov.city;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class CityController {
+	@GetMapping("/")
+	public String index() {
+		return "City Server";
+	}
+}
